ed3df2693205ba7760a93faff2c0369f0fd8f1d4
Sean Redmond <github-smr@sneakemail.com>
2018-05-10 11:45:58 -0400

    Accept extra params

--- lib/searchocle.rb
+++ lib/searchocle.rb
@@ -2,10 +2,16 @@ require "searchocle/version"
 require "nokogiri"
 require "open-uri"
 
-BASE = "http://www.worldcat.org/webservices/catalog/"
+HOST = "worldcat.org"
+BASE = "/webservices/catalog/"
 
 module Searchocle
-  def self.read(oclc, key=nil)
-    Nokogiri::XML(open("%scontent/%s?wskey=%s" % [BASE, oclc, key]))
+  def self.read(oclc, key, options={})
+    options["wskey"] = key
+    url = URI::HTTP.build(host: HOST,
+                          path: "%s%s%s" % [BASE, "content/", oclc],
+                          query: URI.encode_www_form(options))
+    puts url
+    Nokogiri::XML(open(url))
   end
 end
