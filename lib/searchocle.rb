d9b493574a79c4406a521d6d43a0f9961925a208
Sean Redmond <github-smr@sneakemail.com>
2018-05-10 11:51:40 -0400

    Stub out method to filter parameters

--- lib/searchocle.rb
+++ lib/searchocle.rb
@@ -7,11 +7,15 @@ BASE = "/webservices/catalog/"
 
 module Searchocle
   def self.read(oclc, key, options={})
-    options["wskey"] = key
+    options[:wskey] = key
     url = URI::HTTP.build(host: HOST,
                           path: "%s%s%s" % [BASE, "content/", oclc],
-                          query: URI.encode_www_form(options))
+                          query: URI.encode_www_form(filter_params(options)))
     puts url
     Nokogiri::XML(open(url))
+  end
+
+  def self.filter_params(p)
+    p.select{|k, v| [:wskey, :servicelevel, :recordSchema].include?(k)}
   end
 end
