d77d31afcbc1ff29b705ae973fce20fdca769e7a
Sean Redmond <github-smr@sneakemail.com>
2018-05-11 10:34:44 -0400

    Return an enumerator from sru

--- lib/searchocle.rb
+++ lib/searchocle.rb
@@ -1,6 +1,7 @@
 require "searchocle/version"
 require "nokogiri"
 require "open-uri"
+require "pp"
 
 module Searchocle
   HOST = "worldcat.org"
@@ -26,7 +27,27 @@ module Searchocle
     url = URI::HTTP.build(host: HOST,
                           path: "%s%s" % [BASE, "search/worldcat/sru/"],
                           query: URI.encode_www_form(filter_params(:sru, options)))
-    Nokogiri::XML(open(url))
+    page = Nokogiri::XML(open(url))
+
+    return Enumerator.new do |rec|
+      loop do 
+        page.css("record").each do |r|
+          rec << r
+        end
+
+        nrec = page.css("nextRecordPosition")
+        if nrec.empty? or nrec.first.text.empty?
+          break
+        end
+
+        options[:startRecord] = nrec.first.text
+        url = URI::HTTP.build(host: HOST,
+                              path: "%s%s" % [BASE, "search/worldcat/sru/"],
+                              query: URI.encode_www_form(filter_params(:sru, options)))
+        
+        page = Nokogiri::XML(open(url))
+      end
+    end
   end    
 
   def self.filter_params(m, p)
